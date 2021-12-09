f5bf224d385687457a7b62608c03c3aeecd88601
Lavita Zuo <x5zuo@ucsd.edu>
2021-12-08 17:43:06 -0800

    changes

--- examples/exampleColorImageOpenCV.py
+++ examples/exampleColorImageOpenCV.py
@@ -8,6 +8,7 @@ import cv2
 # Path to the module
 # TODO: Modify with the path containing the k4a.dll from the Azure Kinect SDK
 modulePath = 'C:\\Program Files\\Azure Kinect SDK v1.4.1\\sdk\\windows-desktop\\amd64\\release\\bin\\k4a.dll' 
+
 # under x86_64 linux please use r'/usr/lib/x86_64-linux-gnu/libk4a.so'
 # In Jetson please use r'/usr/lib/aarch64-linux-gnu/libk4a.so'
 
